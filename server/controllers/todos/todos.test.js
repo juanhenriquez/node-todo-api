ccd78277b4bda64057192c0a8f08cf8b423e34a4
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-07-06 00:51:26 -0400

    add test to GET /todos

--- server/controllers/todos/todos.test.js
+++ server/controllers/todos/todos.test.js
@@ -4,8 +4,17 @@ const request = require('supertest');
 const app = require('./../../server');
 const Todo = require('./../../models/todo');
 
+const todos = [
+  { text: 'First test todo'},
+  { text: 'Second test todo' }
+]
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe('TODO\'s Routes', () => {
@@ -25,7 +34,7 @@ describe('TODO\'s Routes', () => {
         })
         .end((err, res) => {
           if (err) return done(err);
-          Todo.find()
+          Todo.find({ text })
             .then(todos => {
               expect(todos.length).toBe(1);
               expect(todos[0].text).toBe(text);
@@ -49,7 +58,7 @@ describe('TODO\'s Routes', () => {
 
           Todo.find()
             .then(todos => {
-              expect(todos.length).toBe(0);
+              expect(todos.length).toBe(2);
               done();
             })
             .catch(e => done(e));
@@ -58,12 +67,13 @@ describe('TODO\'s Routes', () => {
   });
 
   describe('GET /todos', () => {
-    it('should fetch all the todos', done => {
+    it('should get all the todos', done => {
       request(app)
         .get('/api/todos')
         .expect(200)
         .expect(res => {
           expect(res.body.todos).toBeA('array');
+          expect(res.body.todos.length).toBe(2);
         })
         .end(done);
     });
