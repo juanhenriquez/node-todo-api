c415c35974bf8d8d3b18846c65c3a0da29c13448
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-07-07 00:17:42 -0400

    setup mongolab enviroment vars

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-app';
+
 mongoose.Promise = global.Promise;
-const connection = mongoose.connect('mongodb://localhost:27017/todo-app', { useMongoClient: true });
+const connection = mongoose.connect(DB_URI, { useMongoClient: true });
 
 module.exports = mongoose;
