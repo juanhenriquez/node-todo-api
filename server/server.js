414573bf33ca67dbbd6538c55f93dd5390d8ec5b
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-07-09 05:34:30 -0400

    config development and test db

--- server/server.js
+++ server/server.js
@@ -4,17 +4,27 @@ const bodyParser = require('body-parser');
 // import handlers
 const { messageValidationErrors, notFoundErrors } = require('./handlers/errorHandlers');
 
+// import and set main routes
+const routes = require('./routes/routes');
+
+// config info
+const env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app';
+} else if (env === 'test'){
+  process.env.PORT = 8080;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app-test';
+}
+const PORT = process.env.PORT;
+
 // create the app
 const app = express();
 
 // set middlewares
 app.use(bodyParser.json());
 
-// config info
-const PORT = process.env.PORT || 3000;
-
-// import and set main routes
-const routes = require('./routes/routes');
+// set root endpoint and app's routes
 app.use('/api', routes);
 
 // error handlers
